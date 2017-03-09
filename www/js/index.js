c8ec61816d937519acf77e6c409a732c5944e2ed
wasura4 <wasura.ediri@gmail.com>
2017-03-09 10:42:24 +0800

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -19,23 +19,17 @@
 var app = {
     // Application Constructor
     initialize: function() {
-        this.bindEvents();
+        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
     },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
+
     // deviceready Event Handler
     //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
+    // Bind any cordova events here. Common events are:
+    // 'pause', 'resume', etc.
     onDeviceReady: function() {
-window.plugins.PushbotsPlugin.initialize("58c03c4d4a9efabb0d8b4567", {"android":{"sender_id":"637464166235"}});
-
+        this.receivedEvent('deviceready');
     },
+
     // Update DOM on a Received Event
     receivedEvent: function(id) {
         var parentElement = document.getElementById(id);
